ef4a4af4626547d96c2477dae33c30076ffdcf39
JakeJMattson <JakeJMattson@gmail.com>
2021-02-02 23:16:25 -0600

    Design mentionEmbed

--- src/main/kotlin/me/jakejmattson/airlock/MainApp.kt
+++ src/main/kotlin/me/jakejmattson/airlock/MainApp.kt
@@ -1,6 +1,10 @@
 package me.jakejmattson.airlock
 
+import dev.kord.common.entity.Snowflake
+import dev.kord.common.kColor
 import me.jakejmattson.discordkt.api.dsl.bot
+import me.jakejmattson.discordkt.api.extensions.addInlineField
+import me.jakejmattson.discordkt.api.extensions.profileLink
 
 fun main(args: Array<String>) {
     val token = args.firstOrNull() ?: return
@@ -8,6 +12,35 @@ fun main(args: Array<String>) {
     bot(token) {
         prefix {
             ">"
+        }
+
+        mentionEmbed {
+            val kord = it.discord.kord
+            val prefix = it.prefix()
+
+            author {
+                val user = kord.getUser(Snowflake(254786431656919051))!!
+
+                icon = user.avatar.url
+                name = user.tag
+                url = user.profileLink
+            }
+
+            title = "Airlock"
+            color = it.discord.configuration.theme?.kColor
+            description = "\nCurrent prefix is `$prefix`" +
+                "\nUse `${prefix}help` to see commands."
+
+            thumbnail {
+                kord.getSelf().avatar.url
+            }
+
+            addInlineField("", "[[Source]](https://github.com/JakeJMattson/AirLock)")
+
+            footer {
+                val versions = it.discord.versions
+                text = "1.0.0 - ${versions.library} - ${versions.kord}"
+            }
         }
     }
 }
